84510c0161fdcc9be8319f2e6bf49e32b1144ecd
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:08:38 +0530

    Added Task Model

--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/TaskManagerApplication.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/TaskManagerApplication.java
@@ -3,7 +3,7 @@ package com.example.Task_Manager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // enable auto configuration and component scanning
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
