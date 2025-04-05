0a7da1b73b4e126304ab8540a6b79f5b7a07f4aa
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:45:08 +0530

    Create getall and getask by id API s

--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/controller/TaskController.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/controller/TaskController.java
@@ -1,4 +1,29 @@
 package com.example.Task_Manager.controller;
 
+import com.example.Task_Manager.model.Task;
+import com.example.Task_Manager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")   // Base URL for all task-related requests.
+@CrossOrigin(origins = "*")      // Allows requests from any domain as an example from my Front end angular app
+
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
+        return taskService.getTaskById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
