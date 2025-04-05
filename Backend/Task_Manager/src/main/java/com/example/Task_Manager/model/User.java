a65522e823051546566ccd96074f523f1c7a36f2
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:27:52 +0530

    Added Lombok Annotaions and intialize the varibles in User.java

--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/User.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/User.java
@@ -1,4 +1,27 @@
 package com.example.Task_Manager.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+
 public class User {
+
+        @Id // Primary key.
+        @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-generates the ID.
+        private Long id;
+
+        @Column(unique = true, nullable = false) // Username must be unique and not null.
+        private String username;
+
+        @Column(nullable = false)  // Password is required.
+        private String password;
 }
+
