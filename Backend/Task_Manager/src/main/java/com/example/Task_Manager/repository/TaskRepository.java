f1d8d8bc6c5e9f81937bbc9b652643151df3a0db
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:30:51 +0530

    Created TaskRepository file

---
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.example.Task_Manager.repository;
+
+import com.example.taskmanager.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository   // Marks this as a Spring bean for data access.
+public interface TaskRepository extends JpaRepository<Task, Long> { }
