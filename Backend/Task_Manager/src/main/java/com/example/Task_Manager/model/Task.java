044cf04678375bdca55af5bf7af550fe9490ad45
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:20:53 +0530

    Added Lombok annotaions to the task.java

--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/Task.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/Task.java
@@ -1,5 +1,19 @@
 package com.example.Task_Manager.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tasks")
+
 public class Task {
 
 }
