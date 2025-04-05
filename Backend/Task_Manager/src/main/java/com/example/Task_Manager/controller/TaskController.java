8e65492fa4bda8ecc7f291dc835199dd71537abc
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 16:42:33 +0530

    Added Task controller file to generate Rest ful API s

---
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/controller/TaskController.java
@@ -0,0 +1,4 @@
+package com.example.Task_Manager.controller;
+
+public class TaskController {
+}
--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/service/UserService.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.Task_Manager.service;
 
+import com.example.Task_Manager.model.User;
+import com.example.Task_Manager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service   // Marks this as a service class.
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        // Encrypt the password before saving for security.
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
