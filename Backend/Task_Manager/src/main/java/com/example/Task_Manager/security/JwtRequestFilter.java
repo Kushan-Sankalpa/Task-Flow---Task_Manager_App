7ad6afd6bc332840f9540be3382b390fb040b932
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 17:01:19 +0530

    Added JwtRequestfilter file to help  secure all endpoint

---
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/security/JwtRequestFilter.java
@@ -0,0 +1,29 @@
+package com.example.Task_Manager.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import io.jsonwebtoken.ExpiredJwtException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+
+
+public class JwtRequestFilter {
+
+    @Component   // Registers this filter so it can be used.
+    public class JwtRequestFilter extends OncePerRequestFilter {
+
+        @Autowired
+        private UserDetailsService userDetailsService;
+
+        @Autowired
+        private JwtUtil jwtUtil;
+}
