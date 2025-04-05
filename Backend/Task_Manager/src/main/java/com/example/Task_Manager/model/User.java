a44def9c4cdb9e1d8f35f94d19c4ace4a2f925e4
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-05 18:03:23 +0530

    do some updates in User model

--- Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/User.java
+++ Backend/Task_Manager/src/main/java/com/example/Task_Manager/model/User.java
@@ -2,13 +2,11 @@ package com.example.Task_Manager.model;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
+
+
+
 @Entity
 @Table(name = "users")
 
@@ -23,5 +21,42 @@ public class User {
 
         @Column(nullable = false)  // Password is required.
         private String password;
+
+        // No-argument constructor
+        public User() {
+        }
+
+        // All-argument constructor
+        public User(Long id, String username, String password) {
+                this.id = id;
+                this.username = username;
+                this.password = password;
+        }
+
+        // Getters and Setters
+
+        public Long getId() {
+                return id;
+        }
+
+        public void setId(Long id) {
+                this.id = id;
+        }
+
+        public String getUsername() {
+                return username;
+        }
+
+        public void setUsername(String username) {
+                this.username = username;
+        }
+
+        public String getPassword() {
+                return password;
+        }
+
+        public void setPassword(String password) {
+                this.password = password;
+        }
 }
 
