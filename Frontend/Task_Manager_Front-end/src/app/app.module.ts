76e1731e7e0436d11de021d55fd08d8bb2d9f2c9
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-06 10:06:24 +0530

    add ngmodule to group each parts together

--- Frontend/Task_Manager_Front-end/src/app/app.module.ts
+++ Frontend/Task_Manager_Front-end/src/app/app.module.ts
@@ -8,3 +8,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
+
+@NgModule({
+    declarations: [
+      AppComponent,        // The root component.
+      LoginComponent,      // The login page.
+      TaskListComponent,   // The page that shows the list of tasks.
+      TaskFormComponent    // The page for adding/editing tasks.
+    ],
+    imports: [
+      BrowserModule,       // Needed for running in a browser.
+      ReactiveFormsModule, // Lets us build reactive forms.
+      HttpClientModule,    // Allows making HTTP calls to the backend.
+      AppRoutingModule     // Enables routing between pages.
+    ],
+    providers: [],
+    bootstrap: [AppComponent] // This tells Angular to start with AppComponent.
+  })
+  export class AppModule { }
