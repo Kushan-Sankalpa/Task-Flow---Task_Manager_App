84a1e3ede33311d580c0b31c82cd911e26b03b54
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-07 00:41:58 +0530

    Finalize the code design

--- Frontend/Task_Manager_Front-end/src/app/components/task-list/task-list.component.ts
+++ Frontend/Task_Manager_Front-end/src/app/components/task-list/task-list.component.ts
@@ -3,34 +3,31 @@ import { TaskService, Task } from '../../services/task.service';
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-task-list',
-    templateUrl: './task-list.component.html',
-    styleUrls: ['./task-list.component.css']
-  })
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];  // list of tasks to be displayed.
 
-  export class TaskListComponent implements OnInit {
-    tasks: Task[] = [];  // list of tasks to be displayed.
-  
-    constructor(private taskService: TaskService, private router: Router) { }
-  
-    ngOnInit(): void {
-      this.loadTasks();
-    }
-  
-    // Loads tasks by calling the TaskService.
-    loadTasks(): void {
-      this.taskService.getTasks().subscribe(data => this.tasks = data);
-    }
-  
-    // Deletes a task and reloads the list.
-    deleteTask(id: number): void {
-      this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
-    }
-  
-    // Navigates to the task form for editing.
-    editTask(id: number): void {
-      this.router.navigate(['/tasks/edit', id]);
-    }
+  constructor(private taskService: TaskService, private router: Router) { }
 
-    
+  ngOnInit(): void {
+    this.loadTasks();
   }
+
+  // Loads tasks by calling the TaskService.
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe(data => this.tasks = data);
+  }
+
+  // Deletes a task and reloads the list.
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
+  }
+
+  // Navigates to the task form for editing.
+  editTask(id: number): void {
+    this.router.navigate(['/tasks/edit', id]);
+  }
+}
