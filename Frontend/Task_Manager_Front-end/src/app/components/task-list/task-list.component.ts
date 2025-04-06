df12ec4dfacecd16e7035aec87128681d84f14ef
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-06 23:57:43 +0530

    Do some modern ui updates

--- Frontend/Task_Manager_Front-end/src/app/components/task-list/task-list.component.ts
+++ Frontend/Task_Manager_Front-end/src/app/components/task-list/task-list.component.ts
@@ -1,34 +1,36 @@
-
 import { Component, OnInit } from '@angular/core';
 import { TaskService, Task } from '../../services/task.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-task-list',
-  templateUrl: './task-list.component.html',
-  styleUrls: ['./task-list.component.css']
-})
-export class TaskListComponent implements OnInit {
-  tasks: Task[] = [];  // List of tasks to display.
+    selector: 'app-task-list',
+    templateUrl: './task-list.component.html',
+    styleUrls: ['./task-list.component.css']
+  })
 
-  constructor(private taskService: TaskService, private router: Router) { }
+  export class TaskListComponent implements OnInit {
+    tasks: Task[] = [];  // list of tasks to be displayed.
+  
+    constructor(private taskService: TaskService, private router: Router) { }
+  
+    ngOnInit(): void {
+      this.loadTasks();
+    }
+  
+    // Loads tasks by calling the TaskService.
+    loadTasks(): void {
+      this.taskService.getTasks().subscribe(data => this.tasks = data);
+    }
+  
+    // Deletes a task and reloads the list.
+    deleteTask(id: number): void {
+      this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
+    }
+  
+    // Navigates to the task form for editing.
+    editTask(id: number): void {
+      this.router.navigate(['/tasks/edit', id]);
+    }
 
-  ngOnInit(): void {
-    this.loadTasks();
+    
   }
-
-  // Loads tasks from the backend.
-  loadTasks(): void {
-    this.taskService.getTasks().subscribe(data => this.tasks = data);
-  }
-
-  // Deletes a task and reloads the list.
-  deleteTask(id: number): void {
-    this.taskService.deleteTask(id).subscribe(() => this.loadTasks());
-  }
-
-  // Navigates to the task form to edit the task.
-  editTask(id: number): void {
-    this.router.navigate(['/tasks/edit', id]);
-  }
-}
