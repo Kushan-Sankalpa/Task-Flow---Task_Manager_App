319db63ff9b28a6253ab9c7f59c4a40f540cdd61
kushan-panda <kushan.20230023@iit.ac.lk>
2025-04-06 10:07:31 +0530

    Added app-routing module file and import modules and components

---
+++ Frontend/Task_Manager_Front-end/src/app/app-routing.module.ts
@@ -0,0 +1,5 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
